d799175297c04a4ac1b02c9dce0e0391f9f3c1c7
machine <steve2008.ma@gmail.com>
2023-06-27 11:23:39 +0800

    修改发布版本

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         'requests>=2.23.0'
     ],
-    version='0.1.7',
+    version='0.3.0',
     url='https://github.com/machine-w/crown',
     author='machine-w',
     author_email='steve2008.ma@gmail.com',
